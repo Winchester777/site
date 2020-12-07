0a27191718a56d17c13f5d96df7ecde78461137c
Winchester777 <75395620+Winchester777@users.noreply.github.com>
2020-12-07 22:24:09 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,41 @@
+
+var btn = document.querySelector('.btn');
+
+btn.onclick = function () {
+	btn.classList.add('pulse');
+}
+
+$(function () {
+	$(window).scroll(function() {
+	    $('.selection_title').each(function(){
+	        var imagePos = $(this).offset().top;
+
+	        var topOfWindow = $(window).scrollTop();
+	        if (imagePos < topOfWindow+650) {
+	            $(this).addClass("fadeInLeft");
+	        }
+	    });
+	});
+
+	$(window).scroll(function() {
+	    $('.advantage').each(function(){
+	        var imagePos = $(this).offset().top;
+
+	        var topOfWindow = $(window).scrollTop();
+	        if (imagePos < topOfWindow+650) {
+	            $(this).addClass("fadeInUp");
+	        }
+	    });
+	});
+
+	$(window).scroll(function() {
+	    $('.ticket').each(function(){
+	        var imagePos = $(this).offset().top;
+
+	        var topOfWindow = $(window).scrollTop();
+	        if (imagePos < topOfWindow+650) {
+	            $(this).addClass("fadeInUp");
+	        }
+	    });
+	});
+})
